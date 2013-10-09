615e2347a6683f9c5ed73987cfa7a3102a5d90cd
Emilien Macchi <emilien.macchi@enovance.com>
2013-10-09 17:03:25 +0200

    Update roles + templates
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

---
+++ roles/identity/keystone-server.pp
@@ -0,0 +1,92 @@
+#
+# Copyright (C) 2013 eNovance SAS <licensing@enovance.com>
+#
+# Authors: Mehdi Abaakouk <mehdi.abaakouk@enovance.com>
+#          Emilien Macchi <emilien.macchi@enovance.com>
+#          Francois Charlier <francois.charlier@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+class os_role_keystone (
+  $local_ip = $ipaddress_eth1,
+){
+
+  $encoded_user = uriescape($os_params::keystone_db_user)
+  $encoded_password = uriescape($os_params::keystone_db_password)
+
+# Configure Keystone
+  class { 'keystone':
+    enabled        => true,
+    package_ensure => 'latest',
+    admin_token    => $os_params::ks_admin_token,
+    compute_port   => "8774",
+    verbose        => false,
+    debug          => false,
+    sql_connection => "mysql://${encoded_user}:${encoded_password}@${os_params::keystone_db_host}/keystone",
+    idle_timeout   => 60,
+# ToDo (EmilienM): Update to PKI tokens
+    token_format   => "UUID",
+  }
+
+  keystone_config {
+    "token/driver":     value => "keystone.token.backends.memcache.Token";
+    "token/expiration": value => "86400";
+    "memcache/servers": value => inline_template("<%= scope.lookupvar('os_params::keystone_memchached').join(',') %>");
+    "ec2/driver":       value => "keystone.contrib.ec2.backends.sql.Ec2";
+    "DEFAULT/syslog_log_facility": value => 'LOG_LOCAL0';
+    "DEFAULT/use_syslog": value => 'yes';
+  }
+
+
+# Keystone Endpoints + Users
+  class { 'keystone::roles::admin': 
+    email => $os_params::ks_admin_email,
+    password => $os_params::ks_admin_password,
+  }
+
+  keystone_role { $os_params::keystone_roles_addons: ensure => present }
+
+  class {"keystone::endpoint":
+    public_address   => $os_params::ks_keystone_public_host,
+    admin_address    => $os_params::ks_keystone_admin_host,
+    internal_address => $os_params::ks_keystone_internal_host,
+    public_port      => $os_params::ks_keystone_public_port,
+    admin_port       => $os_params::keystone_admin_port,
+    internal_port    => $os_params::keystone_port,
+    region           => 'RegionOne',
+    public_protocol  => $os_params::ks_keystone_public_proto
+  }
+
+  class{"swift::keystone::auth":
+    password => $os_params::ks_swift_password,
+    address => $os_params::ks_swift_internal_host,
+    port => $os_params::swift_port,
+    public_address => $os_params::ks_swift_public_host,
+    public_protocol => $os_params::ks_swift_public_proto,
+    public_port => $os_params::ks_swift_public_port
+  }
+
+  class { 'ceilometer::keystone::auth':
+    password         => $os_params::ks_ceilometer_password,
+    public_address   => $os_params::ks_ceilometer_public_host,
+    admin_address    => $os_params::ks_ceilometer_admin_host,
+    internal_address => $os_params::ks_ceilometer_internal_host,
+    public_protocol  => $os_params::ks_ceilometer_public_proto,
+    port             => $os_params::ceilometer_port,
+  }
+
+  class{ 'swift::keystone::dispersion':
+    auth_pass => $os_params::ks_swift_dispersion_password
+  }
+
+}
