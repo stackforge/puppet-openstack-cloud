3fe463c21df78088a3bdf39b0f9730ae68322fd7
Emilien Macchi <emilien.macchi@enovance.com>
2013-10-20 22:11:20 +0200

    Disable Swift
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

--- site.pp
+++ site.pp
@@ -100,13 +100,13 @@ node 'os-ci-test3.enovance.com' inherits common{
     }
 
 ## Object Storage
-    class{'os_role_swift_proxy':
-      local_ip => $ipaddress_eth1,
-    }
-    class {"os_role_swift_ringbuilder":
-       rsyncd_ipaddress => $ipaddress_eth1,
-    }
-    Class["os_role_swift_ringbuilder"] -> Class["os_role_swift_proxy"]
+###    class{'os_role_swift_proxy':
+###      local_ip => $ipaddress_eth1,
+###    }
+###    class {"os_role_swift_ringbuilder":
+###       rsyncd_ipaddress => $ipaddress_eth1,
+###    }
+###    Class["os_role_swift_ringbuilder"] -> Class["os_role_swift_proxy"]
 
 ## Messaging
     class{'os_role_rabbitmq': }
