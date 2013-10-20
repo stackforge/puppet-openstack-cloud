7e4829fc8f2bf0bf9bc22966d5bd53aa801866bb
Emilien Macchi <emilien.macchi@enovance.com>
2013-10-20 21:56:11 +0200

    Revert "Disable Ceilometer Agent Central"
    This reverts commit 9f2fb2895577d86a8d84c0556380357027b84114.

--- site.pp
+++ site.pp
@@ -95,7 +95,7 @@ node 'os-ci-test3.enovance.com' inherits common{
 ## Metering
     class{'os_ceilometer_server':}
     # Enforce using Ceilometer Agent central on one node (should be fixed in Icehouse):
-zsh:1: command not found: q
+    class {"ceilometer::agent::central": }
 
 ## Identity 
     class {"os_keystone_server":
