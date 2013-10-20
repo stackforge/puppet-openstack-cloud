951fbaab469918e8cb3fada4f797de6cbeb8d425
Emilien Macchi <emilien.macchi@enovance.com>
2013-10-20 22:29:29 +0200

    FIx keystone DB
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

--- roles/identity/keystone-server.pp
+++ roles/identity/keystone-server.pp
@@ -22,7 +22,13 @@ class os_keystone_server (
 ){
 
 # Create the DB
-  class { 'keystone::db::mysql': password => $os_params::keystone_db_password, user => $os_params::keystone_db_user }
+  class { 'keystone::db::mysql':
+    password      => $os_params::keystone_db_password
+    user          => $os_params::keystone_db_user
+    dbname        => 'keystone',
+    host          => $os_params::keystone_db_host,
+    allowed_hosts => $os_params::keystone_allowed_hosts,
+  }
 
 # Configure Keystone
   class { 'keystone':
