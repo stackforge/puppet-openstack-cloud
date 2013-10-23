c47a83417e4dc9eb68f49b1baecb95e159241e11
Emilien Macchi <emilien.macchi@enovance.com>
2013-10-23 14:50:29 +0200

    Add ceilometer common to controller
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

--- site.pp
+++ site.pp
@@ -63,6 +63,7 @@ node 'os-ci-test3.enovance.com' inherits common{
     class {"mysql_server":}
 
 ## Metering
+    class{'os_ceilometer_common':}
     class{'os_ceilometer_server':}
     # Enforce using Ceilometer Agent central on one node (should be fixed in Icehouse):
 #    class {"ceilometer::agent::central": }
