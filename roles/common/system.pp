cd5b3dc25f990b50949eba6af7589795907c8ad7
Emilien Macchi <emilien.macchi@enovance.com>
2013-10-28 15:57:42 +0100

    Fix NTP class
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

--- roles/common/system.pp
+++ roles/common/system.pp
@@ -54,8 +54,8 @@ This node is under the control of Puppet ${::puppetversion}.
   }
 
 # NTP
-  class { '::ntp':
-    servers => [ '0.fr.pool.ntp.org', '0.us.pool.ntp.org' ],
+  class { "ntp":
+    ntpservers => [ '0.fr.pool.ntp.org', '0.us.pool.ntp.org' ],
   }
 
 # SSH Keys
