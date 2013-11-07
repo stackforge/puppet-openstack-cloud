d8c4430a8f3bbd837beaf9da4f0bfa66a8898bfd
Sebastien Badia <seb@sebian.fr>
2013-11-07 18:40:10 +0100

    [site] add os_nova_compute

--- site.pp
+++ site.pp
@@ -35,10 +35,10 @@ import 'roles/object-storage/*.pp'
 node common {
 
 # Params
-  class{ 'os_params': }
+  class { 'os_params': }
 
 # Common system configuration
-  class{ 'os_common_system': }
+  class { 'os_common_system': }
 
 }
 
@@ -59,8 +59,8 @@ node 'os-ci-test3.enovance.com' inherits common{
     class {'mysql_server':}
 
 ## Metering
-    class{'os_ceilometer_common':}
-    class{'os_ceilometer_server':}
+    class {'os_ceilometer_common':}
+    class {'os_ceilometer_server':}
     # Enforce using Ceilometer Agent central on one node (should be fixed in Icehouse):
     class {'ceilometer::agent::central': }
 
@@ -70,7 +70,7 @@ node 'os-ci-test3.enovance.com' inherits common{
     }
 
 # Object Storage
-    class{'os_role_swift_proxy':
+    class {'os_role_swift_proxy':
       local_ip => $ipaddress_eth0,
     }
     class {'os_role_swift_ringbuilder':
@@ -79,7 +79,8 @@ node 'os-ci-test3.enovance.com' inherits common{
     Class['os_role_swift_ringbuilder'] -> Class['os_role_swift_proxy']
 
 # Messaging
-    class{'os_role_rabbitmq': }
+    class {'os_role_rabbitmq': }
+
 
 }
 
@@ -87,16 +88,28 @@ node 'os-ci-test3.enovance.com' inherits common{
 node 'os-ci-test8.enovance.com', 'os-ci-test9.enovance.com', 'os-ci-test12.enovance.com' inherits common{
 
 ## Metering
-    class{'os_ceilometer_common':}
+    class {'os_ceilometer_common':}
 
 ## Object Storage
-    class{ 'os_role_swift_storage':
+    class { 'os_role_swift_storage':
         local_ip    => $ipaddress_eth0,
         swift_zone  =>  $os_params::os_swift_zone[$::hostname],
     }
 }
 
+# Load balancer node
+node 'os-ci-test8.enovance.com' inherits common {
+
+## Haproxy
+
+}
+
 # Compute nodes
 node 'os-ci-test13.enovance.com' inherits common {
+
+## Compute
+  class { 'os_nova_compute':
+    local_ip => $ipaddress_eth0,
+  }
 
 }
