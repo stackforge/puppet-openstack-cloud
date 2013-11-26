c5471f0cbad19f39771d901823d2f8ff5d4b29d6
Emilien Macchi <emilien.macchi@enovance.com>
2013-11-26 11:32:26 +0100

    Small fix on puppet-master role
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

--- roles/automation/puppet-master.pp
+++ roles/automation/puppet-master.pp
@@ -14,8 +14,9 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-
-# Puppet Master nodes
+#
+# Puppet Master node
+#
 
 class os_puppet_master{
 
