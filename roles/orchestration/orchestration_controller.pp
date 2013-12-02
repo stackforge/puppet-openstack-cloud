0081978990cc12da16fddecc5d98ca38cee11d57
Emilien Macchi <emilien.macchi@enovance.com>
2013-12-02 18:47:26 +0100

    Heat support
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

--- roles/orchestration/orchestration_controller.pp
+++ roles/orchestration/orchestration_controller.pp
@@ -18,32 +18,42 @@
 #
 # Orchestration controller node
 #
-class os_orchestration_controller {
-  class { 'heat':
-    keystone_host     => $os_params::ks_heat_public_host,
-    keystone_port     => $os_params::ks_heat_public_port,
-    keystone_protocol => $os_params::ks_heat_public_proto,
-    keystone_password => $os_params::ks_heat_password,
-  }
 
-  class { 'heat::api': }
+class os_orchestration_controller(
+  $ks_keystone_public_host  = $os_params::ks_keystone_public_host,
+  $ks_heat_public_host      = $os_params::ks_heat_public_host,
+  $ks_keystone_public_port  = $os_params::ks_keystone_public_port,
+  $ks_keystone_admin_port   = $os_params::ks_keystone_admin_port,
+  $ks_keystone_public_proto = $os_params::ks_keystone_public_proto,
+  $ks_heat_public_proto     = $os_params::ks_heat_public_proto,
+  $ks_heat_password         = $os_params::ks_heat_password,
+  $heat_db_host             = $os_params::heat_db_host,
+  $heat_db_password         = $os_params::heat_db_password
+) {
+
+  $encoded_user = uriescape($heat_db_user)
+  $encoded_password = uriescape($heat_db_password)
 
   class { 'heat':
-    keystone_host     => $os_params::ks_keystone_public_host,
-    keystone_password => $os_params::ks_heat_password,
-    auth_uri          => "${os_params::ks_keystone_public_proto}://${os_params::ks_keystone_public_host}:35357/v2.0",
+    keystone_host     => $ks_keystone_public_host,
+    keystone_port     => $ks_keystone_public_port,
+    keystone_protocol => $ks_keystone_public_proto,
+    keystone_password => $ks_heat_password,
+    auth_uri          => "${ks_keystone_public_proto}://${ks_keystone_public_host}:${ks_keystone_admin_port}/v2.0",
     rabbit_hosts      => $os_params::rabbit_hosts,
     rabbit_password   => $os_params::rabbit_password,
   }
 
+  class { 'heat::api': }
+
   class { 'heat::db':
-    sql_connection => "mysql://${os_params::heat_db_user}:${os_params::heat_db_password}@${os_params::heat_db_host}/heat",
+    sql_connection => "mysql://${encoded_user}:${os_params::encoded_password}@${heat_db_host}/heat"
   }
 
   class { 'heat::engine':
-    heat_metadata_server_url      => "${os_params::ks_heat_public_proto}://${os_params::ks_keystone_public_host}:8000",
-    heat_waitcondition_server_url => "${os_params::ks_heat_public_proto}://${os_params::ks_keystone_public_host}:8000/v1/waitcondition",
-    heat_watch_server_url         => "${os_params::ks_heat_public_proto}://${os_params::ks_keystone_public_host}:8003",
+    heat_metadata_server_url      => "${ks_heat_public_proto}://${ks_keystone_public_host}:8000",
+    heat_waitcondition_server_url => "${ks_heat_public_proto}://${ks_keystone_public_host}:8000/v1/waitcondition",
+    heat_watch_server_url         => "${ks_heat_public_proto}://${ks_keystone_public_host}:8003"
   }
 
 }
