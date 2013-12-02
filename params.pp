a0ee84caba8c0ffae1665a8f089e798cbe027b31
Emilien Macchi <emilien.macchi@enovance.com>
2013-12-02 17:49:46 +0100

    Sort params.pp for better readability
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

--- params.pp
+++ params.pp
@@ -23,14 +23,14 @@
 class os_params {
 
   # General parameters
-  $site_domain = 'enovance.com'
+  $compute = False
+  $debug = False
   $dns_ips = ['10.68.0.2']
+  $install_packages = False
   $os_release = 'havana'
   $region = 'enovance-ci'
+  $site_domain = 'enovance.com'
   $storage = True
-  $compute = False
-  $install_packages = False
-  $debug = False
   $verbose = False
 
   # Root hashed password
@@ -40,38 +40,38 @@ class os_params {
 
   # OpenStack Identity
   $identity_roles_addons = ['SwiftOperator', 'ResellerAdmin']
-  $ks_admin_token = 'iw3feche3JeeYo9mejoohaugai3thohahwo9tiuyoe5Thier8Eiwah8K'
+  $keystone_admin_port = '35357'
+  $keystone_allowed_hosts = ['os-ci-test%', '10.68.0.%']
+  $keystone_db_host = '10.68.0.47'
+  $keystone_db_password = 'H3YPSYaKbKP40DuvHHpdrhYFVpa10A'
+  $keystone_db_user = 'keystone'
+  $keystone_memchached = ['10.68.0.47:11211']
+  $keystone_port = '5000'
   $ks_admin_email = 'dev@enovance.com'
   $ks_admin_password = 'Xokoph5io2aenaoh0nuiquei9aineigo'
   $ks_admin_tenant = 'admin'
-  $keystone_memchached = ['10.68.0.47:11211']
-  $keystone_port = '5000'
-  $keystone_admin_port = '35357'
-  $ks_keystone_public_proto = 'http'
-  $ks_keystone_public_port = '5000'
-  $ks_keystone_public_host = 'os-ci-test3.enovance.com'
-  $ks_keystone_internal_host = 'os-ci-test3.enovance.com'
+  $ks_admin_token = 'iw3feche3JeeYo9mejoohaugai3thohahwo9tiuyoe5Thier8Eiwah8K'
   $ks_keystone_admin_host = 'os-ci-test3.enovance.com'
-  $keystone_db_host = '10.68.0.47'
-  $keystone_db_user = 'keystone'
-  $keystone_db_password = 'H3YPSYaKbKP40DuvHHpdrhYFVpa10A'
-  $keystone_allowed_hosts = ['os-ci-test%', '10.68.0.%']
+  $ks_keystone_internal_host = 'os-ci-test3.enovance.com'
+  $ks_keystone_public_host = 'os-ci-test3.enovance.com'
+  $ks_keystone_public_port = '5000'
+  $ks_keystone_public_proto = 'http'
 
   # Swift
-  $ks_swift_public_proto = 'http'
-  $ks_swift_public_port = '8080'
-  $ks_swift_public_host = 'os-ci-test3.enovance.com'
   $ks_swift_admin_host = 'os-ci-test3.enovance.com'
-  $ks_swift_internal_host = 'os-ci-test3.enovance.com'
-  $swift_port = '8080'
   $ks_swift_dispersion_password = 'aipee1die1eeSohph9yae8eeluthaleu'
+  $ks_swift_internal_host = 'os-ci-test3.enovance.com'
   $ks_swift_password = 'cwnu6Eeph4jahsh5wooch5Panahjaidie8'
-  $swift_cors_allow_origin = 'http://os-ci-test3.enovance.com'
-  $swift_hash_suffix = 'ni2aseiWi8ich3oo'
-  $swift_rsync_max_connections = 5
-  $swift_memchached = ['10.68.0.47:11211']
+  $ks_swift_public_host = 'os-ci-test3.enovance.com'
+  $ks_swift_public_port = '8080'
+  $ks_swift_public_proto = 'http'
   $statsd_host = '127.0.0.1'
   $statsd_port = '4125'
+  $swift_cors_allow_origin = 'http://os-ci-test3.enovance.com'
+  $swift_hash_suffix = 'ni2aseiWi8ich3oo'
+  $swift_memchached = ['10.68.0.47:11211']
+  $swift_port = '8080'
+  $swift_rsync_max_connections = 5
   $os_swift_zone = {
     'os-ci-test8' => 1,
     'os-ci-test9' => 2,
@@ -107,81 +107,80 @@ class os_params {
 
 
   # Neutron
-  $neutron_port = '9696'
-  $ks_neutron_password = 'AJJTg2fSWrE4X4L1rhKJI74njLpFB0'
-  $ks_neutron_public_proto = 'http'
-  $ks_neutron_public_host = 'os-ci-test3.enovance.com'
+  $external_int = 'eth1'
   $ks_neutron_admin_host = 'os-ci-test3.enovance.com'
   $ks_neutron_internal_host = 'os-ci-test3.enovance.com'
-  $neutron_db_host = '10.68.0.47'
-  $neutron_db_user = 'neutron'
-  $neutron_db_password = 'JCywR7Kt52Hi2hJTtpTb6LCGfo243x'
+  $ks_neutron_password = 'AJJTg2fSWrE4X4L1rhKJI74njLpFB0'
+  $ks_neutron_public_host = 'os-ci-test3.enovance.com'
+  $ks_neutron_public_proto = 'http'
   $neutron_allowed_hosts = ['os-ci-test%', '10.60.0.%']
-
+  $neutron_db_host = '10.68.0.47'
+  $neutron_db_password = 'JCywR7Kt52Hi2hJTtpTb6LCGfo243x'
+  $neutron_db_user = 'neutron'
+  $neutron_port = '9696'
   $tunnel_int   = 'eth0'
-  $external_int = 'eth1'
 
   # Nova
-  $nova_port = '8774'
-  $ks_nova_password = 'WeiveKed1Ahmel7ohfieya6daiquIe'
-  $ks_nova_public_proto = 'http'
-  $ks_nova_public_host = 'os-ci-test3.enovance.com'
   $ks_nova_admin_host = 'os-ci-test3.enovance.com'
   $ks_nova_internal_host = 'os-ci-test3.enovance.com'
-  $nova_db_host = '10.68.0.47'
-  $nova_db_user = 'nova'
-  $nova_db_password = 'Ae3tooch8shohNai9ooqueik9gaevo'
+  $ks_nova_password = 'WeiveKed1Ahmel7ohfieya6daiquIe'
+  $ks_nova_public_host = 'os-ci-test3.enovance.com'
+  $ks_nova_public_proto = 'http'
   $nova_allowed_hosts = ['os-ci-test%', '10.60.0.%']
+  $nova_db_host = '10.68.0.47'
+  $nova_db_password = 'Ae3tooch8shohNai9ooqueik9gaevo'
+  $nova_db_user = 'nova'
+  $nova_port = '8774'
 
   # Glance
+  $glance_allowed_hosts = ['os-ci-test%', '10.60.0.%']
+  $glance_db_host = '10.68.0.47'
+  $glance_db_password = 'uYgNjBzjMjv2fD0yD3LqiQQPKEKuXA'
+  $glance_db_user = 'glance'
+  $glance_host = '10.68.0.47'
   $glance_port = '9292'
-  $ks_glance_password = 'WUBDUbox7gDz3GP6EAYWGos9VBPh82'
-  $ks_glance_public_proto = 'http'
-  $ks_glance_public_host = 'os-ci-test3.enovance.com'
   $ks_glance_admin_host = 'os-ci-test3.enovance.com'
   $ks_glance_internal_host = 'os-ci-test3.enovance.com'
-  $glance_host = '10.68.0.47'
-  $glance_db_host = '10.68.0.47'
-  $glance_db_user = 'glance'
-  $glance_db_password = 'uYgNjBzjMjv2fD0yD3LqiQQPKEKuXA'
-  $glance_allowed_hosts = ['os-ci-test%', '10.60.0.%']
+  $ks_glance_password = 'WUBDUbox7gDz3GP6EAYWGos9VBPh82'
+  $ks_glance_public_host = 'os-ci-test3.enovance.com'
+  $ks_glance_public_proto = 'http'
 
   # Ceilometer
   $ceilometer_database_connection = 'mongodb://10.68.0.47/ceilometer'
   $ceilometer_port = '8777'
-  $ks_ceilometer_password = 'eafhafbheafaefaejiiutiu7374aesf3aiNu'
   $ceilometer_secret = 'S45ILumu44rxn5u7spnbJws9XiWomc'
-  $ks_ceilometer_public_proto = 'http'
-  $ks_ceilometer_public_host = 'os-ci-test3.enovance.com'
   $ks_ceilometer_admin_host = 'os-ci-test3.enovance.com'
   $ks_ceilometer_internal_host = 'os-ci-test3.enovance.com'
+  $ks_ceilometer_password = 'eafhafbheafaefaejiiutiu7374aesf3aiNu'
+  $ks_ceilometer_public_host = 'os-ci-test3.enovance.com'
+  $ks_ceilometer_public_proto = 'http'
 
   # Cinder
-  $cinder_port = '8776'
-  $ks_cinder_password = '768JIxDvnrBbwGaRRn5mHjrzz9jNJi'
-  $cinder_db_host = '10.68.0.47'
-  $cinder_db_user = 'cinder'
-  $cinder_db_password = 'BwgPjjqdbxiKvKm5JMaVrCaT8ePBwP'
   $cinder_allowed_hosts = ['os-ci-test%', '10.60.0.%']
+  $cinder_db_host = '10.68.0.47'
+  $cinder_db_password = 'BwgPjjqdbxiKvKm5JMaVrCaT8ePBwP'
+  $cinder_db_user = 'cinder'
+  $cinder_port = '8776'
   $cinder_rbd_pool = 'cinder_volumes'
-  $glance_api_version = '2'
-  $cinder_rbd_user = 'cinder_rbd'
   $cinder_rbd_secret_uuid = '95c98032-ad65-5db8-f5d3-5bd09cd563ef'
-  $ks_cinder_public_proto = 'http'
-  $ks_cinder_public_host = 'os-ci-test3.enovance.com'
+  $cinder_rbd_user = 'cinder_rbd'
+  $glance_api_version = '2'
   $ks_cinder_admin_host = 'os-ci-test3.enovance.com'
   $ks_cinder_internal_host = 'os-ci-test3.enovance.com'
+  $ks_cinder_password = '768JIxDvnrBbwGaRRn5mHjrzz9jNJi'
+  $ks_cinder_public_host = 'os-ci-test3.enovance.com'
+  $ks_cinder_public_proto = 'http'
 
   # Heat
+  $heat_db_host = '10.68.0.47'
+  $heat_db_password = 'rooghah0phe1tieDeixoodo0quil8iox'
+  $heat_db_user = 'heat'
   $heat_port = '8004'
-  $ks_heat_public_proto = 'http'
-  $ks_heat_public_port = '8004'
-  $ks_heat_public_host = 'os-ci-test3.enovance.com'
   $ks_heat_admin_host = 'os-ci-test3.enovance.com'
   $ks_heat_internal_host = 'os-ci-test3.enovance.com'
-  $heat_db_host = '10.68.0.47'
-  $heat_db_user = 'heat'
-  $heat_db_password = 'rooghah0phe1tieDeixoodo0quil8iox'
   $ks_heat_password = 'EIMMvWvDPEvI08ggT2azYMhGdsNXe6'
+  $ks_heat_public_host = 'os-ci-test3.enovance.com'
+  $ks_heat_public_port = '8004'
+  $ks_heat_public_proto = 'http'
 
 }
