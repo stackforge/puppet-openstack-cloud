0b5c2bd8421b67597d7e000bd6b6fb2632df90c7
Sebastien Badia <sebastien.badia@enovance.com>
2013-12-28 00:44:25 +0100

    Re-Enable keystone wsgi backend
    This reverts commit 652091dbd07721470b8b911ae6a6bb6b84f1e316.
    close: #4524

--- roles/identity/identity-controller.pp
+++ roles/identity/identity-controller.pp
@@ -83,8 +83,7 @@ class os_identity_controller (
 
 # Configure Keystone
   class { 'keystone':
-    #FIXME(sbadia) https://dev.ring.enovance.com/redmine/issues/4524
-    enabled          => true,
+    enabled          => false,
     admin_token      => $ks_admin_token,
     compute_port     => '8774',
     debug            => $debug,
@@ -128,14 +127,13 @@ class os_identity_controller (
 
   include 'apache'
 
-  #FIXME(sbadia) https://dev.ring.enovance.com/redmine/issues/4524
-  #class {'keystone::wsgi::apache':
-  #  servername  => $::fqdn,
-  #  admin_port  => $ks_keystone_admin_port,
-  #  public_port => $ks_keystone_public_port,
-  #  workers     => $::processorcount,
-  #  ssl         => false,
-  #}
+  class {'keystone::wsgi::apache':
+    servername  => $::fqdn,
+    admin_port  => $ks_keystone_admin_port,
+    public_port => $ks_keystone_public_port,
+    workers     => $::processorcount,
+    ssl         => false,
+  }
 
   class {'swift::keystone::auth':
     address          => $ks_swift_internal_host,
