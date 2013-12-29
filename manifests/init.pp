5bd6b6fce8eb597ecdffb2fd216ee9263c41cbba
Emilien Macchi <emilien.macchi@enovance.com>
2013-12-29 21:25:45 +0100

    Add init unit test (WIP)
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -44,11 +44,11 @@ This node is under the control of Puppet ${::puppetversion}.
 
 # DNS
   $datacenter = 'ci'
-  class{ 'resolver':
-    dcinfo      => { ci   => $os_params::dns_ips, },
-    domainname  => $os_params::site_domain,
-    searchpath  => $os_params::site_domain,
-  }
+    class{ 'resolver':
+      dcinfo      => { ci   => $os_params::dns_ips, },
+      domainname  => $os_params::site_domain,
+      searchpath  => $os_params::site_domain,
+    }
 
 # NTP
   class { 'ntp': }
