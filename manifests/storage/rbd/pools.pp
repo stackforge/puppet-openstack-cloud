4fe81f5340619b81f91c05d975250249802a68d2
Emilien Macchi <emilien.macchi@enovance.com>
2014-01-02 14:27:17 +0100

    copyright: change 2013 to 2014
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

--- manifests/storage/rbd/pools.pp
+++ manifests/storage/rbd/pools.pp
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2013 eNovance SAS <licensing@enovance.com>
+# Copyright (C) 2014 eNovance SAS <licensing@enovance.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
