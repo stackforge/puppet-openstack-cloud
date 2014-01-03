d2f6fb7811404271b21b21cbcc8c59f3e7770a3e
Emilien Macchi <emilien.macchi@enovance.com>
2014-01-03 14:51:13 +0100

    fix documentation for dashboard unit tests
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

--- spec/classes/privatecloud_dashboard_spec.rb
+++ spec/classes/privatecloud_dashboard_spec.rb
@@ -13,7 +13,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 #
-# Unit tests for os_dashboard
+# Unit tests for privatecloud::dashboard class
 #
 
 require 'spec_helper'
