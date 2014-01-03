f5285a27f279dde5241dbf03f41b9e412de23360
Emilien Macchi <emilien.macchi@enovance.com>
2014-01-03 15:54:29 +0100

    unit test for compute controller
    - add unit tests for compute controller
    - delete unit test of compute class (useless)
    
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

---
+++ spec/classes/privatecloud_compute_controller_spec.rb
@@ -0,0 +1,78 @@
+#
+# Copyright (C) 2014 eNovance SAS <licensing@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Unit tests for privatecloud::compute::controller class
+#
+
+require 'spec_helper'
+
+describe 'privatecloud::compute::controller' do
+
+  shared_examples_for 'openstack compute controller' do
+
+    let :pre_condition do
+      "class { 'privatecloud::compute':
+        nova_db_host            => '10.0.0.1',
+        nova_db_user            => 'nova',
+        nova_db_password        => 'secrete',
+        rabbit_hosts            => ['10.0.0.1'],
+        rabbit_password         => 'secrete',
+        ks_glance_internal_host => '10.0.0.1',
+        glance_port             => '9292',
+        verbose                 => true,
+        debug                   => true }"
+    end
+
+    let :params do
+      { :ks_keystone_internal_host            => '10.0.0.1',
+        :ks_nova_password                     => 'secrete',
+        :api_eth                              => '10.0.0.1',
+        :neutron_metadata_proxy_shared_secret => 'secrete' }
+    end
+
+    it 'configure compute controller' do
+      should contain_class('nova::scheduler').with(:enabled => true)
+      should contain_class('nova::cert').with(:enabled => true)
+      should contain_class('nova::consoleauth').with(:enabled => true)
+      should contain_class('nova::conductor').with(:enabled => true)
+      should contain_class('nova::spicehtml5proxy').with(:enabled => true)
+      should contain_class('nova::api').with(
+          :enabled                              => true,
+          :auth_host                            => '10.0.0.1',
+          :admin_password                       => 'secrete',
+          :api_bind_address                     => '10.0.0.1',
+          :neutron_metadata_proxy_shared_secret => 'secrete'
+        )
+    end
+  end
+
+  context 'on Debian platforms' do
+    let :facts do
+      { :osfamily => 'Debian' }
+    end
+
+    it_configures 'openstack compute controller'
+  end
+
+  context 'on RedHat platforms' do
+    let :facts do
+      { :osfamily => 'RedHat' }
+    end
+
+#TODO(Emilien) Problem with Spice: http://paste.openstack.org/show/pxIyzg5gqeJVR2nUPtoB/
+#    it_configures 'openstack compute controller'
+  end
+
+end
