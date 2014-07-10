e3f134293e219a7ae22ed1b70efb6eb405e3aa54
Yanis Guenane <yanis.guenane@enovance.com>
2014-07-10 14:43:51 -0400

    Fix incorrect logging parameter name

--- manifests/network.pp
+++ manifests/network.pp
@@ -101,10 +101,10 @@ class cloud::network(
   if $use_syslog {
     $log_dir = false
     neutron_config {
-      'DEFAULT/logging_context_format': value => '%(process)d %(levelname)s %(name)s [%(request_id)s %(user_identity)s] %(instance)s%(message)s';
-      'DEFAULT/logging_default_format': value => '%(process)d %(levelname)s %(name)s [-] %(instance)s%(message)s';
-      'DEFAULT/logging_debug_format': value => '%(funcName)s %(pathname)s:%(lineno)d';
-      'DEFAULT/logging_exception_format': value => '%(process)d TRACE %(name)s %(instance)s';
+      'DEFAULT/logging_context_format_string': value => '%(process)d %(levelname)s %(name)s [%(request_id)s %(user_identity)s] %(instance)s%(message)s';
+      'DEFAULT/logging_default_format_string': value => '%(process)d %(levelname)s %(name)s [-] %(instance)s%(message)s';
+      'DEFAULT/logging_debug_format_suffix': value => '%(funcName)s %(pathname)s:%(lineno)d';
+      'DEFAULT/logging_exception_prefix': value => '%(process)d TRACE %(name)s %(instance)s';
     }
   } else {
     $log_dir = '/var/log/neutron'
