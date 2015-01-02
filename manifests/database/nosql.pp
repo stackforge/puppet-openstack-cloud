6892a0814cf757c5aafab32de62323626fe513b9
Yanis Guenane <yanis.guenane@enovance.com>
2015-01-02 05:31:32 -0500

    MongoDB: Add support for Sharding configuration
    This commit adds supports for mongodb sharding configuration.
    Mongodb node can now be set either as a configuration server, a sharding
    server or a regular server (as of today - backward compatibility).
    
    Change-Id: I1e4fff22f08d0b7c54b828c8e61d4d0e8e8f652b

--- manifests/database/nosql.pp
+++ manifests/database/nosql.pp
@@ -40,11 +40,23 @@
 #   Should be an hash.
 #   Default to {}
 #
+# [*mode*]
+#   (optional) Mode in which the mongod server is suppose to run.
+#   Authorized values are : configsvr, shardsvr, false
+#   Defaults to false
+#
+# [*mongos_cfg_server*]
+#   (optional) Array of config server the mongos process will connect to
+#   Sould be an array. Example ['configsvr1:27019', 'configsvr2:27019']
+#   Defaults to []
+#
 class cloud::database::nosql(
   $bind_ip           = '127.0.0.1',
   $nojournal         = false,
   $replset_members   = $::hostname,
   $firewall_settings = {},
+  $mode              = false,
+  $mongos_server     = [],
 ) {
 
   # should be an array
@@ -62,16 +74,33 @@ class cloud::database::nosql(
 
   class { 'mongodb::globals':
     manage_package_repo => $manage_package_repo
-  }->
-  class { 'mongodb':
-    bind_ip   => $array_bind_ip,
-    nojournal => $nojournal,
-    replset   => 'ceilometer',
-    logpath   => '/var/log/mongodb/mongod.log',
   }
 
+  $mongodb_default_params = {
+    'bind_ip'   => $array_bind_ip,
+    'nojournal' => $nojournal,
+    'replset'   => 'ceilometer',
+    'logpath'   => '/var/log/mongodb/mongod.log',
+  }
+
+  case $mode {
+    /configsvr/: {
+      $mongodb_params_real = merge($mongodb_default_params, {'configsvr' => true})
+      $port_real           = 27019
+    }
+    /shardsvr/: {
+      $mongodb_params_real = merge($mongodb_default_params, {'shardsvr' => true})
+      $port_real           = 27018
+    }
+    default : {
+      $mongodb_params_real = $mongodb_default_params
+      $port_real           = 27017
+    }
+  }
+  ensure_ressource('class', 'mongodb', $mongodb_params_real)
+
   exec {'check_mongodb' :
-    command   => "/usr/bin/mongo ${bind_ip}:27017",
+    command   => "/usr/bin/mongo ${bind_ip}:${port_real}",
     logoutput => false,
     tries     => 60,
     try_sleep => 5,
@@ -89,9 +118,20 @@ class cloud::database::nosql(
     require => Exec['check_mongodb'],
   }
 
+  if ! $mongos_cfg_server.empty? {
+    $member = values_at($array_replset_members, 0)
+
+    class {'mongodb::mongos' :
+      configdb => $mongos_cfg_server,
+    }->
+    mongodb_shard {'ceilometer' :
+      member => "ceilometer/${member}:27018",
+    }
+  }
+
   if $::cloud::manage_firewall {
     cloud::firewall::rule{ '100 allow mongodb access':
-      port   => '27017',
+      port   => $port_real,
       extras => $firewall_settings,
     }
   }
