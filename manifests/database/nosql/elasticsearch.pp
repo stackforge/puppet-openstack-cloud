86eb19288539d93802661c5c8885fc0e44ae94cd
Yanis Guenane <yanis.guenane@enovance.com>
2015-02-02 07:42:45 -0500

    Enable HA on logging infrastructure
    This commit aims to allow the loadbalancing for both
    ElasticSearch and Kibana requests.
    
    Change-Id: I6d17b62a8bbbe203dc24100b550bd716d23d2959

--- manifests/database/nosql/elasticsearch.pp
+++ manifests/database/nosql/elasticsearch.pp
@@ -13,14 +13,18 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 #
-# == Class: cloud::logging::server
+# == Class: cloud::database::nosql::elasticsearch
 #
-# [*kibana_port*]
-#   (optional) Port of Kibana service.
-#   Defaults to '8300'
+# Install an ElasticSearch server
 #
-# [*kibana_bind_ip*]
-#   (optional) Address on which kibana is listening on
+# === Parameters:
+#
+# [*listen_port*]
+#   (optional) Port on which ElasticSearch instance should listen
+#   Defaults to '9200'
+#
+# [*listen_ip*]
+#   (optional) IP address on which ElasticSearch instance should listen
 #   Defaults to '127.0.0.1'
 #
 # [*firewall_settings*]
@@ -28,31 +32,25 @@
 #   Should be an hash.
 #   Default to {}
 #
-class cloud::logging::server(
-  $kibana_port           = '8300',
-  $kibana_bind_ip        = '127.0.0.1',
-  $firewall_settings     = {},
-) {
+class cloud::database::nosql::elasticsearch (
+  $listen_port       = '9200',
+  $listen_ip         = '127.0.0.1',
+  $firewall_settings = {},
+){
 
-  Class['cloud::database::nosql::elasticsearch'] -> Class['kibana3']
-  Class['cloud::database::nosql::elasticsearch'] -> Class['cloud::logging::agent']
+  include ::elasticsearch
 
-  include ::kibana3
-  include cloud::database::nosql::elasticsearch
-  include cloud::logging::agent
-  elasticsearch::instance {'fluentd' : }
-
-  @@haproxy::balancermember{"${::fqdn}-kibana":
-    listening_service => 'kibana',
+  @@haproxy::balancermember{"${::fqdn}-es_cluster":
+    listening_service => 'elasticsearch',
     server_names      => $::hostname,
-    ipaddresses       => $kibana_bind_ip,
-    ports             => $kibana_port,
+    ipaddresses       => $listen_ip,
+    ports             => $listen_port,
     options           => 'check inter 2000 rise 2 fall 5'
   }
 
   if $::cloud::manage_firewall {
-    cloud::firewall::rule{ '100 allow kibana access':
-      port   => $kibana_port,
+    cloud::firewall::rule{ '100 allow elasticsearch access':
+      port   => $listen_port,
       extras => $firewall_settings,
     }
   }
