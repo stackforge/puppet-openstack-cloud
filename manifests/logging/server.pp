d54232ac9a183a300ef0fc527d32f54f01c6a13f
Yanis Guenane <yanis.guenane@enovance.com>
2015-02-02 07:42:45 -0500

    Enable HA on logging infrastructure
    This commit aims to allow the loadbalancing for both
    ElasticSearch and Kibana requests.
    
    Change-Id: I6d17b62a8bbbe203dc24100b550bd716d23d2959

--- manifests/logging/server.pp
+++ manifests/logging/server.pp
@@ -15,11 +15,48 @@
 #
 # == Class: cloud::logging::server
 #
-class cloud::logging::server{
+# [*kibana_port*]
+#   (optional) Port of Kibana service.
+#   Defaults to '8300'
+#
+# [*kibana_bind_ip*]
+#   (optional) Address on which kibana is listening on
+#   Defaults to '127.0.0.1'
+#
+# [*elasticsearch_port*]
+#   (optional) Port of ElasticSearch service.
+#   Defaults to '9200'
+#
+# [*elasticsearch_bind_ip*]
+#   (optional) Address on which elasticsearch is listening on
+#   Defaults to '127.0.0.1'
+#
+class cloud::logging::server(
+  kibana_port           = '8300',
+  kibana_bind_ip        = '127.0.0.1',
+  elasticsearch_port    = '9200',
+  elasticsearch_bind_ip = '127.0.0.1',
+) {
 
   include ::elasticsearch
   include ::kibana3
   include cloud::logging::agent
   elasticsearch::instance {'fluentd' : }
+
+  @@haproxy::balancermember{"${::fqdn}-es_cluster":
+    listening_service => 'elasticsearch',
+    server_names      => $::hostname,
+    ipaddresses       => $elasticsearch_bind_ip,
+    ports             => $elasticsearch_port,
+    options           => 'check inter 2000 rise 2 fall 5'
+  }
+
+  @@haproxy::balancermember{"${::fqdn}-kibana":
+    listening_service => 'kibana',
+    server_names      => $::hostname,
+    ipaddresses       => $kibana_bind_ip,
+    ports             => $kibana_port,
+    options           => 'check inter 2000 rise 2 fall 5'
+  }
 
 }
