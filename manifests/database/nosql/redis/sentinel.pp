4b704f5cdc255d3ed9d6394e890ba544b888a6c8
Yanis Guenane <yanis.guenane@enovance.com>
2015-02-12 08:21:17 -0500

    Redis: Enable master failover
    This commit enable redis master failover. After a period of time
    a new master will be elected by the sentinel and the haproxy will route
    the stream to the correct redis node.
    
    Change-Id: Ib107fdf37e9d0a24f9be4ae73c1a1092507f84b6

--- manifests/database/nosql/redis/sentinel.pp
+++ manifests/database/nosql/redis/sentinel.pp
@@ -24,17 +24,36 @@
 #   Used for firewall purpose.
 #   Default to 26379
 #
+# [*haproxy_monitor_ip*]
+#   (optional) IP on which the HAProxy API is listening on
+#   Used for redis master failover purpose
+#   Default to 127.0.0.1
+#
+# [*haproxy_monitor_port*]
+#   (optional) Port on which the HAProxy API is listening on
+#   Used for redis master failover purpose
+#   Default to 10300
+#
 # [*firewall_settings*]
 #   (optional) Allow to add custom parameters to firewall rules
 #   Should be an hash.
 #   Default to {}
 #
 class cloud::database::nosql::redis::sentinel(
-  $port              = 26379,
-  $firewall_settings = {},
+  $port                 = 26379,
+  $haproxy_monitor_ip   = '127.0.0.1',
+  $haproxy_monitor_port = '10300',
+  $firewall_settings    = {},
 ) {
 
   include ::redis::sentinel
+  file { '/bin/redis-notifications.sh':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0750',
+    content => template('cloud/database/redis-notifications.sh.erb'),
+  }
 
   if $::cloud::manage_firewall {
     cloud::firewall::rule{ '100 allow redis sentinel access':
