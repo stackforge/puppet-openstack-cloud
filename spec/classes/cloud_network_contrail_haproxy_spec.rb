e3c424181f94f9244d3cc67db6be55ba09f4a372
Yanis Guenane <yanis.guenane@enovance.com>
2015-06-06 20:32:31 +0200

    contrail: Initial commit
    This commit introduces the management of a new component opencontrail[1].
    
    Opencontrail is a Sofware Defined Network solution. A new approach has
    been taken here to include it. Since not every deployment will need to
    manage an opencontrail solution, every related file have been put into
    'manifests/network/contrail' (including the HA bits). The HA bits are
    then included in loadbalancer.pp and not created (ie. set to false) by
    default.
    
    [1] http://www.opencontrail.org/
    
    Change-Id: I135f575560b0dde707a28e995da5b00b7aa6d765

---
+++ spec/classes/cloud_network_contrail_haproxy_spec.rb
@@ -0,0 +1,65 @@
+#
+# Copyright (C) 2015 eNovance SAS <licensing@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Unit tests for cloud::network::contrail::haproxy
+#
+
+require 'spec_helper'
+
+describe 'cloud::network::contrail::haproxy' do
+
+  shared_examples_for 'contrail-haproxy stanzas' do
+
+    let :params do
+      { }
+    end
+
+    it { is_expected.to contain_cloud__loadbalancer__binding('contrail_analytics_api').with(
+      :ports     => '8081'
+    )}
+
+    it { is_expected.to contain_cloud__loadbalancer__binding('contrail_config_api').with(
+      :ports     => '8082'
+    )}
+
+    it { is_expected.to contain_cloud__loadbalancer__binding('contrail_config_discovery').with(
+      :ports     => '5998'
+    )}
+
+    it { is_expected.to contain_cloud__loadbalancer__binding('contrail_webui_http').with(
+      :ports     => '8079'
+    )}
+
+    it { is_expected.to contain_cloud__loadbalancer__binding('contrail_webui_https').with(
+      :ports     => '8143'
+    )}
+  end
+
+  context 'on Debian platforms' do
+    let :facts do
+      { :osfamily => 'Debian' }
+    end
+
+  end
+
+  context 'on RedHat platforms' do
+    let :facts do
+      { :osfamily => 'RedHat' }
+    end
+
+    it_configures 'contrail-haproxy stanzas'
+  end
+
+end
